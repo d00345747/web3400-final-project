ae08de5ab0740e8b86e33939f35635b72b1101b5
Nikolas White <d00345747@dmail.dixie.edu>
2019-04-19 12:09:54 -0600

    fixed the Sticky nav and made smooth scrolling when linking to the other sections of the page.

--- servicespecialist/footer.php
+++ servicespecialist/footer.php
@@ -2,7 +2,7 @@
                 <img src="../images/ServspecLogoWhite.png" alt="Servic Specialist St. George">
                 <p>Service Specialist © 2019</p>
             </div>
-            <script src="javascript/navbar.js"></script> <!-- for persistance, not working right now. -->
+            <script src="../javascript/navbar.js"></script> <!-- for persistance, not working right now. -->
         </div>
     </body>
 </html>
